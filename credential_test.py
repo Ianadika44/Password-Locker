c2a21a4fbab98e9e1bff42fd0e84d62b9cf25a26
Ianadika44 <adika19ian@gmail.com>
2020-04-20 23:43:25 +0300

    added the teardown method to excecute instructions ran

--- credential_test.py
+++ credential_test.py
@@ -36,11 +36,11 @@ class TestCredential(unittest.TestCase):
         self.new_credential.save_credential()  # saving the new credential
         self.assertEqual(len(Credential.credential_list), 1)
 
-    # def tearDown(self):
-    #     '''
-    #     tearDown method that does clean up after each test case has run.
-    #     '''
-    #     Credential.credential_list = []
+    def tearDown(self):
+        '''
+        tearDown method that does clean up after each test case has run.
+        '''
+        Credential.credential_list = []
 
     def test_save_multiple_credential(self):
         '''
